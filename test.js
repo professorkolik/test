2e67e3714246388813bd9ae79e6eb4960c429d6f
kolik <professor.studio@gmail.com>
2014-11-18 11:31:33 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,24 @@
+element.addEventListener('click', function(e) { console.log(true); })
+
+.addEventListener(, function(e) {
+
+});
+element.addEventListener('click', function(e) {
+  console.log('true');
+});
+
+(function() {
+  "use strict";
+
+
+  var Module = (function() {
+    function Module() {
+
+    }
+    Module.prototype = {
+
+    };
+
+    return Module;
+  }());
+}());
